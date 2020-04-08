cacf3ae47f8ac22beb5afb6bc33c3909f29e4fc1
Tom Cao <itom41098@gmail.com>
2020-04-08 13:55:28 +0700

    API lecturer's invoice

--- routes/invoices.js
+++ routes/invoices.js
@@ -5,6 +5,10 @@ const constant = require('../utils/constant');
 let Course = require('../models/course');
 let User = require('../models/user');
 let Invoice = require('../models/invoice');
+let Lesson = require('../models/lesson');
+let Feedback = require('../models/feedback');
+let Subject = require('../models/subject');
+let Discount = require('../models/discount');
 
 // Get All Invoices
 router.get('/', async (req, res) => {
@@ -54,5 +58,50 @@ router.post('/update', async (req, res) => {
     res.json(null);
   }
 });
+
+
+// Get Lecturer's Invoices
+router.post('/lecturer', async (req, res) => {
+  try {
+    let listInvoices = await Invoice.find();
+    let lecturerID = req.body._id;
+    let listCourses = await Course.find({_idLecturer: lecturerID});
+
+    let result = [];
+    for (let invoice of listInvoices)
+    {
+
+      for (let course of listCourses)
+      {
+
+        if (invoice['_idCourse'].toString() === course['_id'].toString())
+        {
+          let lessons = await Lesson.find({_idCourse: course['_id']});
+          let feedback = await Feedback.find({_idCourse: course['_id']});
+          let lecturer = await User.findById(course['_idLecturer']);
+          let subject = await Subject.findById(course['_idSubject']);
+          let discount = await Discount.find({_idCourse: course['_id']});
+
+          invoice = {
+            ...invoice._doc,
+            subject: subject,
+            lecturer: lecturer,
+            discount: discount,
+            lessons: lessons,
+            feedback: feedback
+          }
+          result.push(invoice);
+          continue;
+        }
+      }
+    }
+
+
+    res.json(result);
+  } catch(e) {
+    res.status(400).json('Error: ' + e);
+  }
+});
+
 
 module.exports = router

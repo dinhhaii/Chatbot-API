e2d068a8068f27905a9c123079aaf0900b682235
Dinh Hai <dinhhhaii@gmail.com>
2020-06-18 23:25:54 +0700

    Update cart

--- routes/cart.js
+++ routes/cart.js
@@ -4,6 +4,7 @@ const modelGenerator = require('../utils/model-generator');
 const Cart = require('../models/cart');
 const Course = require('../models/course');
 const Discount = require('../models/discount');
+const User = require('../models/user');
 
 // Get All Cart
 router.get('/', async (req, res) => {
@@ -26,39 +27,54 @@ router.get('/:idUser', async (req, res) => {
   const { idUser } = req.params;
 
   try {
-    let cart = await Cart.findOne({_idUser: idUser});
-
+    let cart = await Cart.findOne({ _idUser: idUser });
     if (cart) {
-
       let courses = [];
       for (let item of cart.items) {
-        let course = await Course.findById(item['_idCourse']);
-        let discount = await Discount.findById(item['_idDiscount']);
-        let discountList = await Discount.find({ _idCourse: item['_idCourse']});
+        let course = await Course.findById(item["_idCourse"]);
+        let lecturer = await User.findById(course._idLecturer);
+        let discount = await Discount.findById(item["_idDiscount"]);
+        let discountList = await Discount.find({
+          _idCourse: item["_idCourse"],
+        });
+
+        // Get available discount
+        let availableDiscount = null;
+        discountList.forEach((element) => {
+          if (element.status === "available" && !element.isDelete) {
+            availableDiscount = element;
+          }
+        });
+
+        if (!discount || discount.status === "expired" || discount.isDelete) {
+          discount = availableDiscount;
+        }
 
         course = {
-          course: { ...course._doc, discountList: discountList},
+          course: { ...course._doc, discountList: discountList, lecturer },
           discount,
-        }
+        };
 
         if (course) {
           courses.push(course);
+        } else {
+          res.json({ error: "Course not found" });
         }
       }
 
       let result = {
         _id: cart._id,
         _idUser: cart._idUser,
-        items: courses
-      }
+        items: courses,
+      };
 
       res.json(result);
     } else {
-        let cart = await modelGenerator.createCart(idUser, []);
-        res.json(cart);
+      let cart = await modelGenerator.createCart(idUser, []);
+      res.json(cart);
     }
   } catch (e) {
-    res.status(400).json('Error: ' + e);
+    res.status(400).json({ error: e.message });
   }
 });
 
@@ -93,6 +109,83 @@ router.post('/update', async (req, res) => {
       .catch (err => console.log(err));
   } else {
     res.json(null);
+  }
+});
+
+router.post('/add-coupon', async (req, res) => {
+  const { idUser, code } = req.body;
+  try {
+    let cart = await Cart.findOne({_idUser: idUser});
+    let isUpdated = false;
+    console.log(cart.items);
+    if (cart) {
+      let courses = [];
+      const { items } = cart;
+      for (let index in items) {
+        const item = items[index];
+
+        let course = await Course.findById(item._idCourse);
+        let discount = await Discount.findById(item._idDiscount);
+        let discountList = await Discount.find({ _idCourse: item._idCourse });
+
+        // Get available discount
+        let availableDiscount = null;
+        let newDiscount = null;
+
+        discountList.forEach(element => {
+          if (!element.isDelete) {
+            if (element.status === "available") {
+              availableDiscount = element;
+            }
+            if (element.code.toLowerCase() === code.toLowerCase() && element.status !== 'expired') {
+              newDiscount = element;
+            }
+          }
+        });
+
+        if ((!discount || discount.status === 'expired' || discount.isDelete) && availableDiscount) {
+          discount = availableDiscount;
+          items[index]._idDiscount = availableDiscount._id;
+          isUpdated = true;
+        };
+
+        if (newDiscount) {
+          discount = newDiscount;
+          items[index]._idDiscount = newDiscount._id.toString();
+          isUpdated = true;
+        }
+
+        course = {
+          course: { ...course._doc, discountList: discountList },
+          discount,
+        }
+
+        if (course) {
+          courses.push(course);
+        } else {
+          res.json({ error: "Course not found" });
+        }
+      }
+
+      let result = {
+        _id: cart._id,
+        _idUser: cart._idUser,
+        items: courses,
+        isUpdated,
+      }
+
+      const cartUser = await Cart.findOne({ _idUser: idUser });
+      cartUser['items'] = [ ...items ];
+      await cartUser.save();
+
+      res.json(result);
+    } else {
+      let cart = await modelGenerator.createCart(idUser, []);
+      res.json(cart);
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ error: e.message });
   }
 });
 
